ab96c10715d9c191bd36358c1b77bd03103a9d98
VDrews <thedrews@outlook.es>
2020-02-02 18:51:21 +0100

    Design homepage

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,11 +1,27 @@
 <template lang="pug">
-  v-app(light)
-    v-card.pa-3(flat)
+  v-app(light, style="background-color: #FAFAFA")
+    v-card.pa-3(flat, style="position: fixed; top: 0; left: 0; right: 0; z-index: 20", color="transparent")
       v-layout(align-center, justify-space-between)
-        img(src="@/assets/Routeleo.png", style="height: 40px")
-        v-icon mdi-menu
+        v-flex(v-if="$vuetify.breakpoint.mdAndUp", xs12, md8)
+        v-flex(xs12, md4)
+          v-layout.px-4(align-center, justify-space-between)
+            img(src="@/assets/Routeleo.png", style="height: 40px")
+            v-icon mdi-menu
     v-content
-      nuxt
+      v-layout(style="height: 100vh;")
+        v-flex(v-if="$vuetify.breakpoint.mdAndUp", xs12, md8)
+          v-img.pa-8.align-end(:src="require('~/static/img/cover.jpg')", style="width: 100%; height: 100%; object-fit: cover;")
+            v-layout
+              v-chip.mr-3(color="white")
+                v-icon mdi-airplane
+              v-chip.mr-3(color="white")
+                v-icon mdi-train
+              v-chip.mr-3(color="white")
+                v-icon mdi-bus
+            h1.display-4.font-weight-bold.white--text El precio ya no es una excusa.
+            h2.white--text Con Routeleo elige la aventura y olvidate del precio
+        v-flex.pa-4(xs12, md4, style="overflow-y: scroll")
+          nuxt.mt-12
 </template>
 
 <script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,33 +1,66 @@
 <template lang="pug">
   #index
-    v-layout
-      v-flex(v-if="$vuetify.breakpoint.mdAndUp", xs12, md8)
-        img(src="/img/cover.jpg", style="width: 100%; height: 100%; object-fit: cover;")
-      v-flex.px-4(xs12, md4)
-        v-card.pa-3(style="border-radius: 24px")
-          v-text-field(solo, flat, placeholder="Origen", style="font-size: 1.2em")
-          v-divider
-          v-text-field(solo, flat, placeholder="Destino", style="font-size: 1.2em")
-          v-layout.px-4(justify-space-between)
-            .grey--text(style="font-size: 1.2em") 16 Fec
-            v-divider(vertical)
-            .grey--text(style="font-size: 1.2em") Añadir vuelta
-          v-btn.mt-4.font-weight-bold(rounded, block, dark, color="teal lighten-2") Busca el mejor precio
-        v-layout.mt-6(justify-space-between, align-center)
-          b Los más buscados
-          v-btn.text--overline(text, small) VER MAS
-        v-layout.mt-4.hide-overflow
-          div.pa-4.mr-2(v-for="i in 6", :key="i", style="background-image: url('/img/madrid.jpg'); border-radius: 16px; min-width: 160px")
-            div.font-weight-bold.white--text(style="font-size: 1.6em; margin-top: 140px") Madrid
-            div.font-weight-bold.white--text(style="font-size: 1.2em") Desde <b>48€</b>
-        b.mt-12 Selección de Routeleo
+    v-card.pa-3(style="border-radius: 24px")
+      v-text-field(solo, flat, placeholder="Origen", style="font-size: 1.2em")
+      v-divider
+      v-text-field(solo, flat, placeholder="Destino", style="font-size: 1.2em")
+      v-layout.px-4(justify-space-between)
+        v-menu(v-model="menuida", offset-y)
+          template(v-slot:activator="{ on }")
+            .grey--text(style="font-size: 1.2em", v-on="on") {{ idaComputed }}
+          v-date-picker(v-model="ida", color="teal", no-title, scrollable, @input="menuida = false", locale="es")
+        v-divider(vertical)
+        v-menu(v-model="menuvuelta", offset-y)
+          template(v-slot:activator="{ on }")
+            .grey--text(style="font-size: 1.2em", v-on="on") {{ vueltaComputed }}
+          v-date-picker(v-model="vuelta", color="teal", no-title, scrollable, :allowed-dates="allowedDates", @input="menuvuelta = false", locale="es")
+            v-btn(outlined, color="red", @click="vuelta = null") Quitar vuelta
+      v-btn.mt-4.font-weight-bold(rounded, block, dark, color="teal") Busca el mejor precio
+    v-layout.mt-6(justify-space-between, align-center)
+      b Los más buscados
+      v-btn.text--overline(text, small) VER MAS
+    v-layout.my-4.hide-overflow
+      v-img.pa-2.mr-2(v-for="i in 6", :key="i", :src="require('~/static/img/madrid.jpg')", style="width: 150px; border-radius: 16px", gradient="to top right, rgba(100,115,201,.33), rgba(25,32,72,.7)")
+        div.font-weight-bold.white--text(style="font-size: 1.6em; margin-top: 140px") Madrid
+        div.font-weight-bold.white--text(style="font-size: 1.2em") Desde <b>48€</b>
+    b.mb-2 Selección de Routeleo
+    v-card.mt-2(v-for="i in 6", :key="i", style="border-radius: 16px;")
+      v-img.align-end(:src="require('~/static/img/madrid.jpg')", gradient="to top right, rgba(100,115,201,.33), rgba(25,32,72,.7)")
+        v-card-title.white--text.font-weight-bold Madrid
+      v-layout.pa-4(justify-space-between, align-center)
+        b 23 dic - 5 ene
+        v-chip.font-weight-bold.white--text(color="teal") 30€
+
     
 
 </template>
 
 <script>
+import moment from 'moment'
 
 export default {
+  data() {
+    return {
+      ida: new Date().toISOString().substr(0, 10),
+      vuelta: null,
+      menuida: false,
+      menuvuelta: false
+    }
+  },
+
+methods: {
+  allowedDates (val) {return new Date(val) >= new Date(this.ida)}
+},
+
+computed: {
+      idaComputed () {
+        return this.ida ? moment(this.ida).format('DD/MM/YYYY') : ''
+      },
+      vueltaComputed () {
+        return this.vuelta ? moment(this.vuelta).format('DD/MM/YYYY') : 'Añadir vuelta'
+      },
+    },
+
   head() {
     return {
       script: [{ src: 'https://identity.netlify.com/v1/netlify-identity-widget.js' }],
